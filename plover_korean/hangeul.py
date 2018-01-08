e6fc73957f88b90041a16c7481c55c3320a66e04
Nicholas Markopoulos <nsmarkop@gmail.com>
2018-01-07 20:50:18 -0600

    Add new particles to meta plugins and update naming scheme to be more workable for non-basic particles.

---
+++ plover_korean/hangeul.py
@@ -0,0 +1,116 @@
+'''
+Korean script related helper functions or wrappers.
+'''
+
+import hgtk
+
+
+def is_letter(letter: str) -> bool:
+    '''
+    Checks if the provided string is a single Korean letter.
+
+    :param letter: Letter to check.
+    :type letter: str
+
+    :return: If the string is a Korean letter.
+    :rtype: bool
+    '''
+
+    return hgtk.checker.is_jamo(letter)
+
+def is_hangeul(word: str) -> bool:
+    '''
+    Checks if the provided string is in the Korean script.
+
+    :param word: Word to check.
+    :type word: str
+
+    :return: If the string is in the Korean script.
+    :rtype: bool
+    '''
+
+    return hgtk.checker.is_hangul(word)
+
+def get_last_syllable(word: str) -> str:
+    '''
+    Gets the last syllable block of the given word.
+
+    :param word: The word to use.
+    :type word: str
+
+    :return: The last syllable block of the word.
+    :rtype: str
+    '''
+
+    if len(word) >= 1 and is_hangeul(word):
+        last_syllable = word[-1]
+    else:
+        last_syllable = ''
+
+    return last_syllable
+
+def ends_in_vowel(word: str) -> bool:
+    '''
+    Checks if the given word ends in a vowel.
+
+    :param word: The word to check.
+    :type word: str
+
+    :return: If the word ends in a vowel.
+    :rtype: bool
+    '''
+
+    last_syllable = get_last_syllable(word)
+
+    try:
+        is_vowel = last_syllable and not hgtk.checker.has_batchim(last_syllable)
+    except:
+        is_vowel = False
+
+    return is_vowel
+
+def ends_in_consonant(word: str) -> bool:
+    '''
+    Checks if the given word ends in a consonant.
+
+    :param word: The word to check.
+    :type word: str
+
+    :return: If the word ends in a consonant.
+    :rtype: bool
+    '''
+
+    last_syllable = get_last_syllable(word)
+
+    try:
+        is_consonant = last_syllable and hgtk.checker.has_batchim(last_syllable)
+    except:
+        is_consonant = False
+
+    return is_consonant
+
+def is_last_consonant(word: str, letter_to_match: str) -> bool:
+    '''
+    Checks if the given word ennds in the provided consonant.
+
+    :param word: The word to check.
+    :type word: str
+
+    :param letter_to_match: The letter to check for as the last consonant.
+    :type letter_to_match: str
+
+    :return: If the word ends in the given consonant.
+    :rtype: bool
+    '''
+
+    last_syllable = get_last_syllable(word)
+
+    try:
+        is_match = (last_syllable and
+                    is_letter(letter_to_match) and
+                    ends_in_consonant(word) and
+                    hgtk.letter.decompose(last_syllable)[-1] == letter_to_match)
+    except:
+        is_match = False
+
+    return is_match
--- plover_korean/meta.py
+++ plover_korean/meta.py
@@ -3,13 +3,21 @@ Meta plugins for Korean support in Plover.
 '''
 
 from collections import namedtuple
-import hgtk
+import plover_korean.hangeul as hangeul
 
 
-RULE_PARTICLE_TOPIC = '는,은'
-RULE_PARTICLE_SUBJECT = '가,이'
-RULE_PARTICLE_OBJECT = '를,을'
-RULE_PARTICLE_AND_FORMAL = '와,과'
+# Constants for particle rules.
+RULE_PARTICLE_NEUN = '는,은'
+RULE_PARTICLE_GA = '가,이'
+RULE_PARTICLE_REUL = '를,을'
+
+RULE_PARTICLE_DA = '다,이다'
+RULE_PARTICLE_YA = '야,아'
+
+RULE_PARTICLE_WA = '와,과'
+RULE_PARTICLE_RANG = '랑,이랑'
+
+RULE_PARTICLE_RO = '로,으로,ㄹ'
 
 ParticleRuleInfo = namedtuple(
     'ParticleRuleInfo', [
@@ -17,30 +25,54 @@ ParticleRuleInfo = namedtuple(
         'vowel',
         # The particle to use when the previous word ends in a consonant.
         'consonant',
-        # For what last consonant of the previous word should the
+        # For what final consonant of the previous word should the
         # 'consonant' case be treated as the 'vowel' case.
         # Example: 'ㄹ' for the consonant = '으로', vowel = '로' particle.
         'except_consonant'
     ]
 )
 
-def parse_particle_args(meta_args: str) -> ParticleRuleInfo:
-    args_list = meta_args.split(',', 2)
+def parse_particle_args(particle_info: str) -> ParticleRuleInfo:
+    '''
+    Parses raw particle information into a ParticleRuleInfo structure.
+
+    :param particle_info: The generic particle information.
+                          Comma-delimited string format corresponding to ParticleRuleInfo
+                          like 'vowel,consonant,except_consonant'. Both 'vowel' and
+                          'consonant' must be provided.
+    :type particle_info: str
+
+    :return: The parsed particle rule info if possible to parse.
+    :rtype: ParticleRuleInfo
+
+    :raises ValueError: Unable to parse the provided particle information.
+    '''
+
+    args_list = particle_info.split(',', 2)
 
     try:
         vowel = args_list[0]
+        if not hangeul.is_hangeul(vowel):
+            vowel = ''
     except IndexError:
         vowel = ''
 
     try:
         consonant = args_list[1]
+        if not hangeul.is_hangeul(consonant):
+            consonant = ''
     except IndexError:
         consonant = ''
 
     try:
         except_consonant = args_list[2]
+        if not hangeul.is_letter(except_consonant):
+            except_consonant = ''
     except IndexError:
         except_consonant = ''
+
+    if not vowel and not consonant:
+        raise ValueError('Both a vowel and consonant particle must be provided.')
 
     return ParticleRuleInfo(
         vowel=vowel,
@@ -48,47 +80,166 @@ def parse_particle_args(meta_args: str) -> ParticleRuleInfo:
         except_consonant=except_consonant
     )
 
+def format_unresolved_particle(rule_info: ParticleRuleInfo):
+    '''
+    When a choice of one particle cannot be made, formats the choices for output.
+
+    :param rule_info: The particle rule info in use.
+    :type rule_info: ParticleRuleInfo
+
+    :return: Formatted string for output.
+    :rtype: str
+    '''
+
+    return f'{rule_info.consonant}({rule_info.vowel})'
+
 def apply_particle_generic(context, meta_args: str):
+    '''
+    Attaches the provided generic particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :param meta_args: The arguments containing the generic particle information.
+                      Comma-delimited string format corresponding to ParticleRuleInfo
+                      like 'vowel,consonant,except_consonant'. Both 'vowel' and
+                      'consonant' must be provided.
+    :type meta_args: str
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    try:
+        rule_info = parse_particle_args(meta_args)
+    except ValueError:
+        return context.new_action()
+
     action = context.new_action()
     action.prev_attach = True
-
-    rule_info = parse_particle_args(meta_args)
+    action.next_attach = True
 
     last_word = context.last_action.word or ''
-    last_syllable = ''
-    if len(last_word) >= 1:
-        last_syllable = last_word[-1]
 
-    # TODO: Order here is pretty strict and fragile - could probably be refactored
-    can_determine_particle = (last_syllable and
-                              hgtk.checker.is_hangul(last_syllable))
-
-    if (can_determine_particle and
-            hgtk.checker.has_batchim(last_syllable) and
-            (hgtk.letter.decompose(last_syllable)[-1] != rule_info.except_consonant)):
+    if (hangeul.ends_in_consonant(last_word) and
+            not hangeul.is_last_consonant(last_word, rule_info.except_consonant)):
         action.text = rule_info.consonant
-    elif can_determine_particle:
+    elif hangeul.ends_in_vowel(last_word):
         action.text = rule_info.vowel
-    # TODO: Is this good default behavior? Should it just choose one?
     else:
-        action.text = f'{rule_info.consonant}({rule_info.vowel})'
-
-    # For now, build into every particle that a space follows it and it attaches
-    # to the next word (the last part is to be consistent with the workaround
-    # of Plover's automatic spacing done elsewhere)
-    action.text = action.text + ' '
-    action.next_attach = True
+        action.text = format_unresolved_particle(rule_info)
 
     return action
 
-def apply_particle_topic(context, _):
-    return apply_particle_generic(context, RULE_PARTICLE_TOPIC)
+def apply_particle_neun(context, _):
+    '''
+    Attaches the topic particle to the last word in the context
+    to be executed as the next action.
 
-def apply_particle_subject(context, _):
-    return apply_particle_generic(context, RULE_PARTICLE_SUBJECT)
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
 
-def apply_particle_object(context, _):
-    return apply_particle_generic(context, RULE_PARTICLE_OBJECT)
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
 
-def apply_particle_and_formal(context, _):
-    return apply_particle_generic(context, RULE_PARTICLE_AND_FORMAL)
+    return apply_particle_generic(context, RULE_PARTICLE_NEUN)
+
+def apply_particle_ga(context, _):
+    '''
+    Attaches the subject particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_GA)
+
+def apply_particle_reul(context, _):
+    '''
+    Attaches the object particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_REUL)
+
+def apply_particle_da(context, _):
+    '''
+    Attaches the verb / copula particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_DA)
+
+def apply_particle_ya(context, _):
+    '''
+    Attaches the vocative name particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_YA)
+
+def apply_particle_wa(context, _):
+    '''
+    Attaches the formal 'and' particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_WA)
+
+def apply_particle_rang(context, _):
+    '''
+    Attaches the informal 'and' particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_RANG)
+
+def apply_particle_ro(context, _):
+    '''
+    Attaches the direction particle to the last word in the context
+    to be executed as the next action.
+
+    :param context: The context of actions in Plover.
+    :type context: plover.formatting._Context
+
+    :return: The next action for Plover to perform.
+    :rtype: plover.formatting._Action
+    '''
+
+    return apply_particle_generic(context, RULE_PARTICLE_RO)
