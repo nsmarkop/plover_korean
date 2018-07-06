d79fdde53f7dd9a920ef40666b2a8a865b5e1c42
Nicholas Markopoulos <nsmarkop@gmail.com>
2018-07-06 00:15:47 -0500

    Add placeholder for unit tests

---
+++ test/test_cas_util.py
@@ -0,0 +1,16 @@
+''' Unit tests for Korean CAS utility functions '''
+
+import operator
+
+import pytest
+from plover_korean.cas.util import (
+    compare_numeric_text,
+    get_stroke_groups
+)
+
+
+class TestCompareNumericText(object):
+    pass
+
+class TestGetStrokeGroups(object):
+    pass
